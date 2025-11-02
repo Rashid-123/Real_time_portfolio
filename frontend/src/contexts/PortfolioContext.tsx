16118c35bc5191e92eff0f94f431620e540e6a67
Rashid-123 <shadanrashid786@gmail.com>
2025-11-03 03:20:51 +0530

    Deploying

--- frontend/src/contexts/PortfolioContext.tsx
+++ frontend/src/contexts/PortfolioContext.tsx
@@ -1,5 +1,4 @@
 
-
 "use client";
 
 import {
@@ -68,14 +67,36 @@ export function PortfolioProvider({ children }: { children: ReactNode }) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  // --------- Calling the next.js server route
-  const API_ROUTE = "/api/portfolio";
+  const CACHE_KEY = "portfolio_cache";
+  const CACHE_TIME = 15000;
 
-  async function fetchData() {
+  async function fetchData(force = false) {
     try {
-      const res = await fetch(API_ROUTE);
+    
+        //------------- HERE first i am checking for cached data
+      const cached = localStorage.getItem(CACHE_KEY);
+      if (cached && !force) {
+        const { data: cachedData, timestamp } = JSON.parse(cached);
+        const isFresh = Date.now() - timestamp < CACHE_TIME;
+
+        if (isFresh) {
+          setData(cachedData);
+          setLoading(false);
+          return; 
+        }
+      }
+
+    
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/portfolio`);
       if (!res.ok) throw new Error("Failed to fetch portfolio data");
       const newData: PortfolioData = await res.json();
+
+     
+      localStorage.setItem(
+        CACHE_KEY,
+        JSON.stringify({ data: newData, timestamp: Date.now() })
+      );
+
       setData(newData);
       setError(null);
       setLoading(false);
@@ -87,7 +108,7 @@ export function PortfolioProvider({ children }: { children: ReactNode }) {
 
   useEffect(() => {
     fetchData();
-    const interval = setInterval(fetchData, 15000);
+    const interval = setInterval(() => fetchData(true), CACHE_TIME); // refresh every 15s
     return () => clearInterval(interval);
   }, []);
 
