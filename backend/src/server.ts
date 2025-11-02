a07ced9c7d493642b2ea622d21b3f5a60c48f16d
Rashid-123 <shadanrashid786@gmail.com>
2025-11-03 03:52:52 +0530

    added cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -22,7 +22,7 @@ app.use(limiter);
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000", 
+    origin: process.env.FRONTEND_URL || "https://portfolio-theta-inky-tbm0md45x0.vercel.app/" , 
     credentials: true, 
   })
 );
